e7a416c14418741196addc8e279a02fc70377e53
Keroflux <49621236+Keroflux@users.noreply.github.com>
2022-03-28 21:34:54 +0200

    bedre

--- Trend.gd
+++ Trend.gd
@@ -1,10 +1,10 @@
-extends ColorRect
+extends Node2D
 
 var trend_label = preload("res://TrendLabel.tscn")
 var trend_line = preload("res://TrendLine.tscn")
 var trend_color = [Color(1.0, 1.0, 1.0), Color(0.0, 0.0, 1.0), Color(1.0, 0.0, 0.0), Color(0.0, 1.0, 0.0), Color(0.0, 1.0, 1.0), Color(1.0, 1.0, 0.0), Color(1.0, 0.0, 1.0)]
-onready var trends = [get_parent().get_parent().get_node("Blokkdiagram/ProsessVerdi"), get_parent().get_parent().get_node("Blokkdiagram/Setpunkt"),get_parent().get_parent().get_node("Blokkdiagram/Pådrag"),get_parent().get_parent().get_node("Blokkdiagram/Ventil")]
-onready var label_panel = get_parent().get_node("PanelContainer/VSplitContainer/VBoxContainer")
+onready var trends = [get_parent().get_node("Blokkdiagram/ProsessVerdi"), get_parent().get_node("Blokkdiagram/Setpunkt"), get_parent().get_node("Blokkdiagram/Pådrag"), get_parent().get_node("Blokkdiagram/Ventil")]
+onready var label_panel = $PanelContainer2/HSplitContainer/PanelContainer/VSplitContainer/VBoxContainer
 
 
 func _ready() -> void:
@@ -14,29 +14,40 @@ func _ready() -> void:
 		t.default_color = trend_color[i]
 		t.data_source = trends[i]
 		
-		l.get_child(0).color = trend_color[i]
-		l.get_child(0).connect("color_changed", t, "change_color")
+		l.get_child(0).get_child(0).color = trend_color[i]
+		l.get_child(0).get_child(0).connect("color_changed", t, "change_color")
 		l.get_node("Config/VBoxContainer/Max").connect("text_entered", t, "change_max")
 		l.get_node("Config/VBoxContainer/Min").connect("text_entered", t, "change_min")
+		l.get_node("Config/VBoxContainer/Max").text = str(t.maxValue)
+		l.get_node("Config/VBoxContainer/Min").text = str(t.minValue)
 		l.get_node("Config/VBoxContainer/Slett").connect("pressed", self, "remove_trend", [t, l])
-		l.text = "  " + trends[i].type
+		l.get_child(0).get_child(1).text = trends[i].type
 		
 		label_panel.add_child(l)
-		$TrendLines.add_child(t)
-		get_parent().get_node("Timer").connect("timeout", t, "_on_Timer_timeout")
+		$PanelContainer2/HSplitContainer/Trend/TrendLines.add_child(t)
+		get_node("Timer").connect("timeout", t, "_on_Timer_timeout")
 
 
 func _on_Timer_timeout() -> void:
-	for i in $TrendLines.get_child_count():
-		$TrendLines.get_child(i).redraw_trend(trends[i].trend)
+	for i in $PanelContainer2/HSplitContainer/Trend/TrendLines.get_child_count():
+		$PanelContainer2/HSplitContainer/Trend/TrendLines.get_child(i).redraw_trend(trends[i].trend)
 
 
 func add_trend(data_point):
 	var t = trend_line.instance()
 	var l = trend_label.instance()
 	t.data_source = data_point
-	$TrendLines.add_child(t)
+#	l.get_child(0).get_child(0).color = trend_color[i]
+	l.get_child(0).get_child(0).connect("color_changed", t, "change_color")
+	l.get_node("Config/VBoxContainer/Max").connect("text_entered", t, "change_max")
+	l.get_node("Config/VBoxContainer/Min").connect("text_entered", t, "change_min")
+	l.get_node("Config/VBoxContainer/Max").text = str(t.maxValue)
+	l.get_node("Config/VBoxContainer/Min").text = str(t.minValue)
+	l.get_node("Config/VBoxContainer/Slett").connect("pressed", self, "remove_trend", [t, l])
+	l.get_child(0).get_child(1).text = data_point.type
+	$PanelContainer2/HSplitContainer/Trend/TrendLines.add_child(t)
 	label_panel.add_child(l)
+	get_node("Timer").connect("timeout", t, "_on_Timer_timeout")
 
 
 func remove_trend(trend, label):
@@ -45,4 +56,21 @@ func remove_trend(trend, label):
 
 
 func _on_Bakgrunn_color_changed(c):
-	color = c
+	$PanelContainer2/HSplitContainer/Trend.color = c
+
+
+func _on_Button_pressed():
+	if not $PanelContainer.visible:
+		$PanelContainer.show()
+		for tag in get_tree().get_nodes_in_group("Trend"):
+			var a = Button.new()
+			a.text = tag.type
+			a.connect("pressed", self, "add_trend", [tag])
+			$PanelContainer/VBoxContainer/VBoxContainer.add_child(a)
+
+
+func _on_Lukk_pressed():
+	$PanelContainer.hide()
+	for child in $PanelContainer/VBoxContainer/VBoxContainer.get_children():
+		child.queue_free()
+	
